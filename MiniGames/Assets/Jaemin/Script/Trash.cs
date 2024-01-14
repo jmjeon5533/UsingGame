c73dd6074f9ecf9f732482c772be849a96a8ee18
jmjeon5533 <jmjeon5533@gmail.com>
2024-01-14 18:23:55 +0900

    Trash division game Complete

---
+++ MiniGames/Assets/Jaemin/Script/Trash.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trash : MonoBehaviour
+{
+    public string trashName;
+    public Sprite trashSprite;
+    public TrashClick.TrashType trashType;
+}

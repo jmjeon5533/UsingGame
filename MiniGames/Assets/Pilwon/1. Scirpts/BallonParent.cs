3ff1e5c3307323e736a54008ca311354fe2d06db
sangwon0127 <pilwon070127@naver.com>
2024-01-15 13:45:41 +0900

    Box Game System Dev

--- MiniGames/Assets/Pilwon/1. Scirpts/BallonParent.cs
+++ MiniGames/Assets/Pilwon/1. Scirpts/BallonParent.cs
@@ -1,22 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class BallonParent : MonoBehaviour
 {
     public List<Ballon> ballons = new List<Ballon>();
+    [SerializeField] float speed;
 
     private void Start()
     {
         ballons.AddRange(GetComponentsInChildren<Ballon>());
+        StartCoroutine(StartMove());
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (!BallonPopGame.instance.isCurStageClear) return;
+        transform.position += Vector3.right * speed * Time.deltaTime;
     }
 
     public void StageClearCheck()
     {
         if(ballons.Count <= 0) 
         {
-            gameObject.SetActive(false);
-
             if (BallonPopGame.instance.stageLevel == BallonPopGame.instance.stages.Length - 1)
             {
                 Debug.Log("Stage Clear!!!");
@@ -25,7 +37,41 @@ public class BallonParent : MonoBehaviour
             {
                 BallonPopGame.instance.stageLevel++;
                 BallonPopGame.instance.Init();
+                BallonPopGame.instance.isCurStageClear = true;
+                BallonPopGame.instance.isConveyorMove = true;
             }
+        }
+    }
+
+    private void OnEnable()
+    {
+        BallonPopGame.instance.isGameStart = false;
+        StartCoroutine(MoveWait());
+    }
+
+    private IEnumerator MoveWait()
+    {
+        yield return new WaitForSeconds(1.0f);
+        transform.DOMove(new Vector3(0, -1.5f, 0), 2.0f);
+        yield return new WaitForSeconds(1.0f);
+        BallonPopGame.instance.isGameStart = true;
+    }
+
+    private IEnumerator StartMove()
+    {
+        yield return new WaitForSeconds(1.0f);
+        BallonPopGame.instance.isConveyorMove = true;
+        yield return new WaitForSeconds(2.0f);
+        BallonPopGame.instance.isConveyorMove = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Stage"))
+        {
+            BallonPopGame.instance.isCurStageClear = false;
+            BallonPopGame.instance.isConveyorMove = false;
+            gameObject.SetActive(false);
         }
     }
 }
